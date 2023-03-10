12b65b215ba2fc7afb768bb4142f8ceb4e483e95
psg9790 <psg9790@gmail.com>
2023-03-10 11:06:30 +0900

    Newtonsoft Json.net

---
+++ Assets/NewBehaviourScript1.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Newtonsoft.Json;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
