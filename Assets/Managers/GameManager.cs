278218adf0a426dc90d4ac9c210c180d5b0c033f
psg9790 <psg9790@gmail.com>
2023-03-10 18:12:36 +0900

    some managers

---
+++ Assets/Managers/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if(instance == null){
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }else{
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Managers/InputManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager instance;
+
+    private void Awake() {
+        if(instance == null){
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }else{
+            Destroy(this.gameObject);
+        }
+    }
+}
