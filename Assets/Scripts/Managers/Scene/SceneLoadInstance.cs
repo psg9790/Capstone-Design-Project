f25935d3bc0b162324f4bec9e16bd6806b475689
psg9790 <psg9790@gmail.com>
2023-03-20 15:41:25 +0900

    mini settings panel

--- Assets/Scripts/Managers/Scene/SceneLoadInstance.cs
+++ Assets/Scripts/Managers/Scene/SceneLoadInstance.cs
@@ -8,9 +8,12 @@ using DG.Tweening;
 
 // UI에 드래그해서 적용하기 쉽게 씬마다 배치할 프래팹을 따로 만들었음
 // 배열에 이 씬에서 움직일 수 있는 씬을 미리 정해놓으면 그 씬들로 이동가능함
+// 세팅창 키는것도 만들까
 public class SceneLoadInstance : MonoBehaviour
 {
     [SerializeField] private SceneName[] movableScenes;
+    private SettingsInstance settings;
+    [SerializeField] private GameObject settingsPrefab;
     
     public void SwitchScene(string tgt)
     {
@@ -38,6 +41,21 @@ public class SceneLoadInstance : MonoBehaviour
         }
         // 씬 찾기 실패시
         Debug.LogWarning("Scene " + tgt + " not setted");
+    }
+
+    public void OpenSettingsWindow()
+    {
+        if (ReferenceEquals(settings, null))
+        {
+            settings = Instantiate(settingsPrefab).GetComponent<SettingsInstance>();
+        }
+
+        settings.gameObject.SetActive(true);
+    }
+
+    public void CloseSettingsWindow()
+    {
+        settings.CancelChangesButton();
     }
 }
 
