6b6d6544448491cb55a68bfdafd031f5b26f0fc2
daersh <daersh@naver.com>
2023-04-02 15:07:10 +0900

    4.2 업데이트
    1. NPC 가까이 접근 시 바라봄
    2. NPC 영역 내에서 F키 누르면 대화창 나옴.
    3. R키를 통해 다음 대화창으로 이동.
    4. 미니맵 업데이트

--- Assets/Scripts/NPC/NPC_INPUT_MANAGER.cs
+++ Assets/Scripts/NPC/NPC_INPUT_MANAGER.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//NPC의 대사를 저장
 public class NPC_INPUT_MANAGER : MonoBehaviour
 {
     Dictionary<int, string[]> talkData;
-
-    Dictionary<int, string[]> selectData;
     
     // Start is called before the first frame update
     void Awake()
     {
         talkData = new Dictionary<int, string[]>();
-        selectData = new Dictionary<int, string[]>();
         
         GenerateData();
     }
@@ -20,24 +17,27 @@ public class NPC_INPUT_MANAGER : MonoBehaviour
     void GenerateData()
     {
         //id = 1 : SIMSANG
-        talkData.Add(1, new string[] { "오 심상치 않은데?" });
-        talkData.Add(2, new string[] { "중요한 건 꺾이지 않은 마음" });
-        talkData.Add(3, new string[] { "딸깍... 딸깍... 캐리.... 기모리~" });
-        talkData.Add(4, new string[] { "넣을게~" });
-        talkData.Add(5, new string[] { "딸깍... 딸깍... 캐리.... 기모리~" });
+        talkData.Add(1, new string[] { "1", "2","3","4" });
+        talkData.Add(2, new string[] { "1", "2","3","4" });
+        talkData.Add(3, new string[] {"1", "2","3","4" });
+        talkData.Add(4, new string[] {"1", "2","3","4" });
 
-        selectData.Add(6, new string[] { "창고\n창고2" });
-        selectData.Add(7, new string[] { "구매\n판매" });
-        selectData.Add(8, new string[]{"a\nb", "aa"});
+        talkData.Add(6, new string[] { "창고입니다.","창고","창고"});
+        talkData.Add(7, new string[] { "상점입니다.","구매", "판매" });
+        talkData.Add(8, new string[]{"ab", "aa"});
     }
 
     public string GetTalk(int id, int talkIndex) //Object의 id , string배열의 index
     {
-        return talkData[id][talkIndex]; //해당 아이디의 해당
+        if (talkIndex == talkData[id].Length)
+        {
+            return null;
+        }else
+        {
+            return talkData[id][talkIndex]; //해당 아이디의 해당
+        }
+        
     }
 
-    public string getTalk(int id, int talkIndex)
-    {
-        return selectData[id][talkIndex];
-    }
+   
 }
--- Assets/Scripts/NPC/NPC_data.cs
+++ Assets/Scripts/NPC/NPC_data.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class NPC_data : MonoBehaviour
 {
+    public string NPCName;
     public int id;
     public bool isNPC;
     public bool select1;
