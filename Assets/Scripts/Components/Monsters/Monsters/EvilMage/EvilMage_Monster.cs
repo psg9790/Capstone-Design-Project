d4769fc72d13c6c101e0f32c46f3026ca65f499a
psg9790 <psg9790@gmail.com>
2023-04-02 14:53:42 +0900

    added mage monster basicMagic

--- Assets/Scripts/Components/Monsters/Monsters/EvilMage/EvilMage_Monster.cs
+++ Assets/Scripts/Components/Monsters/Monsters/EvilMage/EvilMage_Monster.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EvilMage_Monster : RangedMonster
 {
+    public GameObject basicMagic;
     protected override void OnAwake()
     {
         base.OnAwake();
@@ -12,5 +13,12 @@ public class EvilMage_Monster : RangedMonster
     protected override void OnUpdate()
     {
         base.OnUpdate();
+    }
+
+    public void GenerateBaseMagic()
+    {
+        BasicMagic magic = Instantiate(basicMagic).GetComponent<BasicMagic>();
+        magic.Init(this.transform.position + transform.forward * 0.25f + Vector3.up, 
+            transform.forward, 2.5f);
     }
 }
