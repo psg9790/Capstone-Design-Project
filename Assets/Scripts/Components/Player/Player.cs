afdf789122b9fde772f3f056f08b78ff5588a8f1
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2023-04-26 23:29:44 +0900

    player kill

--- Assets/Scripts/Components/Player/Player.cs
+++ Assets/Scripts/Components/Player/Player.cs
@@ -42,6 +42,7 @@ public class Player : MonoBehaviour
     {
         if (instance == null)
         {
+            UnityEngine.Debug.Log("Player Awake");
             instance = this;
             weaponManager = new WeaponManager(rightHand);
             weaponManager.unRegisterWeapon = (weapon) => { Destroy(weapon); };
--- Assets/Scripts/Managers/Dungeon/DungeonCreator.cs
+++ Assets/Scripts/Managers/Dungeon/DungeonCreator.cs
@@ -14,24 +14,6 @@ public class DungeonCreator : MonoBehaviour
 
     private void Awake()
     {
-        // spawnPoint = GameObject.Find("SpawnPoint");
-        // if (spawnPoint == null)
-        // {
-        //     Debug.LogError("플레이어 스폰 지점이 없습니다... \"SpawnPoint\"오브젝트를 플레이어 생성을 원하는 곳에 배치해주세요.");
-        // }
-        // if (playerPrefab == null)
-        // {
-        //     Debug.LogError("소환할 플레이어 프리팹을 지정해주세요...");
-        // }
-        // else
-        // {
-        //     player = Instantiate(playerPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation)
-        //         .GetComponent<Player>();
-        // }
-    }
-
-    private void Start()
-    {
         spawnPoint = GameObject.Find("SpawnPoint");
         if (spawnPoint == null)
         {
@@ -46,6 +28,11 @@ public class DungeonCreator : MonoBehaviour
             player = Instantiate(playerPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation)
                 .GetComponent<Player>();
         }
+    }
+
+    private void Start()
+    {
+        
 
         if (Camera.main != null)
         {
