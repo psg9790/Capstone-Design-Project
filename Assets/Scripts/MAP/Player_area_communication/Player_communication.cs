5f3d226133270f3a62ecf38d28996a8731b4e4e7
daersh <daersh@naver.com>
2023-05-04 18:24:34 +0900

    ㅁ
    ㅁ

--- Assets/Scripts/MAP/Player_area_communication/Player_communication.cs
+++ Assets/Scripts/MAP/Player_area_communication/Player_communication.cs
@@ -5,77 +5,117 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
+
 public class Player_communication : MonoBehaviour
 {
-    public GameObject ListUI;
-    
-    public TMP_Text Text;
-    public Text text;
-    private string objectTag;
-
+    /*
     //overlap sphere 사용하기 위한것
     public Collider[] colliders;
     public float radius = 1.0f;
-    List<GameObject> dataList = new List <GameObject>();
+    */
+     
+    public List<GameObject> dataList = new List <GameObject>();
     
-    private TMP_Text tmp;
     public RectTransform content;
     public GameObject listItemPrefab;
 
     private Transform[] childList;
-    // Start is called before the first frame update
-    void Start()
-    {
-         
-    }
-
-    // Update is called once per frame
+    
      void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Treasure") || other.CompareTag("Item") || other.CompareTag("NPC"))
         {
-            scan(other);
+            dataList.Add(other.transform.gameObject);
+            StartCoroutine(Scanner());
+            
         }
+        
     }
-
-     void scan(Collider other)
+     
+     void OnTriggerExit(Collider other)
      {
          
-         for (int i = 0; i < content.transform.childCount; i++)
+         if (other.CompareTag("Treasure") || other.CompareTag("Item") || other.CompareTag("NPC"))
          {
-             Debug.Log(content.transform.childCount +"삭제");
-             Destroy(content.GetChild(0).gameObject);
-             dataList.RemoveAt(0);
+             
+             scanInit();
+             ShowData();
+             
+         }
+             
+     }
+
+     void scanInit()
+     {
+         int count= content.transform.childCount;
+         
+         
+             Debug.Log("리스트 제거:" +count);
+             for (int i = 1; i < count; i++)
+             {
+                 Destroy(content.transform.GetChild(1).gameObject);
+             }
+         
+
+         int dataCount = dataList.Count;
+         if (dataCount != 0)
+         {
+             Debug.Log("데이터 제거:" +dataCount);
+             dataList.RemoveRange(0, dataCount);
+         }
+     }
+
+     void ShowData()
+     {
+         int i = 0;
+         for (i = 0; i < dataList.Count; i++)
+         {
+             
+             GameObject item = Instantiate(listItemPrefab, content);
+             TMP_Text insert = item.GetComponentInChildren<TMP_Text>();
+             Debug.Log("리스트생성");
+             insert.text = dataList[i].tag+" 줍기" ;
          }
          
+     }
+     
+     /*
+     void scan()
+     {
+         scanInit();
          //radius를 기준으로 구 안에 있는 콜라이덛를 검출함
          colliders =
-             Physics.OverlapSphere(this.transform.position, radius);
-         
+             Physics.OverlapSphere(transform.position, radius);
          foreach (Collider col in colliders)
          {
              //콜라이더의 테그를 인식하여 이에 맞는 표현 보이기
              if (col.CompareTag("Treasure") || col.CompareTag("Item") || col.CompareTag("NPC"))
              {
-                 dataList.Add(other.gameObject);
+                 dataList.Add(col.gameObject);
              }
-             
          }
+         
+     }
+     */
 
-         for (int i = 0; i < dataList.Count; i++)
-         {
-             GameObject item = Instantiate(listItemPrefab, content);
-             TMP_Text Insert = item.GetComponentInChildren<TMP_Text>();
-             Insert.text = dataList[i].tag;
-         }
+     
+     IEnumerator Scanner()
+     {
+       
+         yield return  new WaitForSeconds(0.5f);
+         
+         yield return  new WaitForSeconds(0.2f);
+         
+         yield return  new WaitForSeconds(0.2f);
+         
      }
 
-     void OnTriggerExit(Collider other)
-         {
-             if (other.CompareTag("Treasure") || other.CompareTag("Item") || other.CompareTag("NPC"))
-             {
-                 scan(other);
-             }
-             
-         }
+     
+     /*
+     private void OnDrawGizmosSelected()
+     {
+         Gizmos.color = Color.red;
+         Gizmos.DrawWireSphere(transform.position, radius);
+     }
+     */
 }
---
+++ Assets/Scripts/MAP/overlapSphere.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+public class overlapSphere : MonoBehaviour
+{
+    public float radius = 2.0f;
+    public Collider[] colliders;
+    
+    public List<GameObject> dataList = new List <GameObject>();
+    public RectTransform content;
+    public GameObject ListItemPrefab;
+
+    private void Start()
+    {
+        InitData();
+    }
+
+    private void Update()
+    {
+        //데이터 초기화하여 List<gameobject> 싹 비운 후 overlapsphere로 리스트 추가
+        InitData();
+        
+        //radius를 기준으로 구 안에 있는 콜라이덛를 검출함
+        colliders =
+            Physics.OverlapSphere(transform.position, radius);
+        
+        foreach (Collider col in colliders)
+        {
+            //콜라이더의 테그를 인식하여 이에 맞는 표현 보이기
+            if (col.CompareTag("Treasure") || col.CompareTag("Item") || col.CompareTag("NPC"))
+            {
+                Debug.Log(col.gameObject.name+"추가");
+                dataList.Add(col.gameObject);
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Treasure") || other.CompareTag("Item") || other.CompareTag("NPC"))
+        {
+            //목록 생성 후 데이터 추가
+            GenerateContent();
+            AddData();
+        }
+    }
+
+    public void OnTriggerExit (Collider other)
+    {
+        //콜라이더 하나 벗어나면 datalist 초기화
+        if (other.CompareTag("Treasure") || other.CompareTag("Item") || other.CompareTag("NPC"))
+        {
+            //목록창 하나 제거 후 업데이트
+            DeleteContent();
+            UpdateData();
+        }
+            
+    }
+
+    void InitData()
+    {
+        //데이터 리스트 초기화
+        int i ;
+        int dataListCount = dataList.Count;
+        UnityEngine.Debug.Log("리스트 갯수: "+dataListCount);
+        dataList.Clear();
+        UnityEngine.Debug.Log("리스트 제거 후 남은 갯수: "+ dataList.Count);
+    }
+
+    
+    //콘텐츠 아래 목록들 보이도록 생성
+    void GenerateContent()
+    {
+        //ㅇ소 생성
+            GameObject Item = Instantiate(ListItemPrefab, content);
+            /*
+            Item.GetComponentInChildren<TMP_Text>().text = dataList[i].name;
+            UnityEngine.Debug.Log("생성된 목록: " + Item.name);
+            */
+            
+    }
+    //목록에 데이터 추가
+    void AddData()
+    {
+        int ListCount = content.childCount;
+        for (int i = 0; i < ListCount; i++)
+        {
+            TMP_Text insert = content.GetChild(i).GetComponentInChildren<TMP_Text>();
+            if (dataList.Count > i ) insert.text = dataList[i].name;
+        }
+    }
+    void UpdateData()
+    {
+        int ContentCount = content.childCount;
+        int dataListCount = dataList.Count;
+        
+        if (ContentCount > 0 )
+        {
+            for (int i = 0; i < ContentCount; i++)
+            {
+                if (dataListCount> i && content.GetChild(i) != null) 
+                {
+                    TMP_Text insert = content.GetChild(i).GetComponentInChildren<TMP_Text>();
+                    insert.text = dataList[i].name;
+                }
+                else if (dataList.Count <= i && content.GetChild(i) != null) 
+                {
+                    Destroy(content.GetChild(i).gameObject);
+                }
+                
+            }
+        }
+    }    
+    
+    void DeleteContent()
+    {
+        int ContentCount = content.childCount;
+        Debug.Log("content 자식 수:" + ContentCount);
+        if (ContentCount > 0)
+        {
+            Destroy(content.GetChild(0).gameObject);
+        }
+    }
+    
+}
