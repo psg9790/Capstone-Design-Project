e130da20ac688fa6f2160875df03206f58fd33b5
psg9790 <psg9790@gmail.com>
2023-05-04 16:50:00 +0900

    added boss1 baseAttack

--- Assets/Scripts/Monsters/SkillSet/SkillSet_Boss1_Nightmare.cs
+++ Assets/Scripts/Monsters/SkillSet/SkillSet_Boss1_Nightmare.cs
@@ -7,10 +7,18 @@ namespace Monsters.Skill
 {
     public class SkillSet_Boss1_Nightmare : SkillSet
     {
+
+        public HitBox baseAttack;
+        
+        void BaseAttack()
+        {
+            HitBox ba = Instantiate(baseAttack);
+            ba.Particle_Play(heart);
+        }
         public override void DoPossibleEngage()
         {
             // throw new System.NotImplementedException();
-            Debug.Log("attack");
+            // Debug.Log("attack");
             monster.animator.SetTrigger("Skill00"); // 사용 가능한 스킬이 없으면 그냥 평타
         }
     }
