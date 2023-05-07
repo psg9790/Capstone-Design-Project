af9140d47a2ea52ebbc02ac85e2ba202b1b54724
daersh <daersh@naver.com>
2023-05-07 22:21:14 +0900

    5.7 상호작용 완성
    ㅈㄱㄴ

--- Assets/Scripts/MAP/overlapSphere.cs
+++ Assets/Scripts/MAP/overlapSphere.cs
@@ -2,18 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class overlapSphere : MonoBehaviour
 {
     public float radius = 2.0f;
 
     public List<GameObject> dataList = new List<GameObject>();
+    
     public RectTransform content;
     private GameObject Incontents;
     private GameObject ClearContents;
     private int MAX = 50;
+    public List<ItemDataPractice> ItemData = new List<ItemDataPractice>();
+    
     private void Start()
     {
     }
@@ -31,22 +36,29 @@ public class overlapSphere : MonoBehaviour
         foreach (Collider col in colliders)
         {
             //콜라이더의 테그를 인식하여 이에 맞는 표현 보이기
-            if (col.CompareTag("Treasure") || col.CompareTag("Item") || col.CompareTag("NPC"))
+            if (col.CompareTag("Item") )
             {
                 Debug.Log(col.gameObject.name + "추가");
                 dataList.Add(col.gameObject);
-                
                 Incontents = content.GetChild(count).gameObject;
                 Incontents.SetActive(true);
-                Incontents.GetComponentInChildren<TMP_Text>().text = dataList[count].name;
+                Incontents.GetComponentInChildren<TMP_Text>().text = dataList[count].GetComponent<FeildItem>().item.itemName;
+                 
                 count++;
-                
             }
         }
         ClearContent(count);
 
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ItemData.Add(dataList[0].GetComponent<FeildItem>().item);
+            Destroy(dataList[0].gameObject);
+            UnityEngine.Debug.Log("Destroyed");
+        }
     }
 
+    
+    
     void ClearContent(int count)
     {
         for(int i= count;i<10;i++)
@@ -58,96 +70,25 @@ public class overlapSphere : MonoBehaviour
             }
         }
     }
+
+    public void AddItem()
+    {
+        
+        GameObject clickButton = EventSystem.current.currentSelectedGameObject;
+        
+        UnityEngine.Debug.Log("클릭된 버튼 번호: "+ clickButton.name);
+        
+        int buttonNum = Int32.Parse(clickButton.name);
+
+        ItemDataPractice _item = dataList[0].GetComponent<FeildItem>().item;
+        
+        ItemData.Add(_item);
+        
+    }
+
+    
 }
 
-/*
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Treasure") || other.CompareTag("Item") || other.CompareTag("NPC"))
-        {
-            //목록 생성 후 데이터 추가
-            GenerateContent();
-            AddData();
-        }
-    }
 
-    public void OnTriggerExit (Collider other)
-    {
-        //콜라이더 하나 벗어나면 datalist 초기화
-        if (other.CompareTag("Treasure") || other.CompareTag("Item") || other.CompareTag("NPC"))
-        {
-            //목록창 하나 제거 후 업데이트
-            DeleteContent();
-            UpdateData();
-        }
-            
-    }
-
-    void InitData()
-    {
-        //데이터 리스트 초기화
-        int i ;
-        int dataListCount = dataList.Count;
-        UnityEngine.Debug.Log("리스트 갯수: "+dataListCount);
-        dataList.Clear();
-        UnityEngine.Debug.Log("리스트 제거 후 남은 갯수: "+ dataList.Count);
-    }
-
-    
-    //콘텐츠 아래 목록들 보이도록 생성
-    void GenerateContent()
-    {
-        //ㅇ소 생성
-            GameObject Item = Instantiate(ListItemPrefab, content);
-            /*
-            Item.GetComponentInChildren<TMP_Text>().text = dataList[i].name;
-            UnityEngine.Debug.Log("생성된 목록: " + Item.name);
-            
-            
-    } */
-    /*
-    //목록에 데이터 추가
-    void AddData()
-    {
-        int ListCount = content.childCount;
-        for (int i = 0; i < ListCount; i++)
-        {
-            TMP_Text insert = content.GetChild(i).GetComponentInChildren<TMP_Text>();
-            if (dataList.Count > i ) insert.text = dataList[i].name;
-        }
-    }
-    void UpdateData()
-    {
-        int ContentCount = content.childCount;
-        int dataListCount = dataList.Count;
-        
-        if (ContentCount > 0 )
-        {
-            for (int i = 0; i < ContentCount; i++)
-            {
-                if (dataListCount> i && content.GetChild(i) != null) 
-                {
-                    TMP_Text insert = content.GetChild(i).GetComponentInChildren<TMP_Text>();
-                    insert.text = dataList[i].name;
-                }
-                else if (dataList.Count <= i && content.GetChild(i) != null) 
-                {
-                    Destroy(content.GetChild(i).gameObject);
-                }
-                
-            }
-        }
-    }    
-    
-    void DeleteContent()
-    {
-        int ContentCount = content.childCount;
-        Debug.Log("content 자식 수:" + ContentCount);
-        if (ContentCount > 0)
-        {
-            Destroy(content.GetChild(0).gameObject);
-        }
-    }
-    */
     
 
