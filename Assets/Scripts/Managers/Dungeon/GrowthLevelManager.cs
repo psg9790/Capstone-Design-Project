91adc6daedaf95d20df576f65587ce459e33bfdf
psg9790 <psg9790@gmail.com>
2023-05-14 16:09:02 +0900

    removed some codes, committed codes

--- Assets/Scripts/Managers/Dungeon/GrowthLevelManager.cs
+++ Assets/Scripts/Managers/Dungeon/GrowthLevelManager.cs
@@ -9,25 +9,25 @@ using UnityEngine.Events;
 
 public class GrowthLevelManager : MonoBehaviour
 {
-    private static GrowthLevelManager instance;
+    private static GrowthLevelManager instance; // 싱글톤
     public static GrowthLevelManager Instance => instance;
 
-    public GameObject playerPrefab; // 플레이어 없으면 생성을 위함
-    private Vector3 playerSpawnPoint;
+    public GameObject playerPrefab; // 플레이어 없으면 생성용
+    private Vector3 playerSpawnPoint; // 코드 내부에서 새로운 스폰포인트 지정용
 
-    [ReadOnly] public int worldLevel; // 월드레벨, 처음엔 0
-    [ReadOnly] public int curWorldMapType;
-    [ReadOnly] public int curLevelMobCount = 0;
-    [HideInInspector] public UnityEvent decreaseMobCount;
-
-    public Transform dungeon1_spawnPoint;
-    public Transform maze_spawnPoint;
-    public int mazeIndent = 28;
-    public int maxMazeBlockCount = 12;
+    [ReadOnly] public int worldLevel; // 월드 레벨(난이도), 처음엔 0
+    [ReadOnly] public int curWorldMapType; // 0~10까지 확률적으로 맵 생성
     
-    public GameObject maze_parent;
-    public NavMeshSurface maze_parent_nav;
-    private RandomMazeGenerator randomMazeGenerator;
+    [ReadOnly] public int curLevelMobCount = 0; // 현재 레벨 완료 및 보스몬스터 생성?을 위한 현재 몹 마릿수
+    [HideInInspector] public UnityEvent decreaseMobCount; // 몹 갯수 줄이는 이벤트
+
+    public Transform dungeon1_spawnPoint; // 던전 1에서 스폰될 위치
+    public int mazeIndent = 28; // 미로 블럭들 사이의 간격
+    public int maxMazeBlockCount = 12; // 최대로 생성할 미로 방의 개수
+    
+    public GameObject maze_parent; // 동적 생성된 미로 오브젝트 관리용
+    public NavMeshSurface maze_parent_nav; // 동적 네브메시 생성용
+    private RandomMazeGenerator randomMazeGenerator; // 랜덤 생성기 클래스
 
     
     private void Awake()
@@ -35,8 +35,8 @@ public class GrowthLevelManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            InitGrowthDungeon();
-            decreaseMobCount.AddListener(DecreaseMobCount);
+            InitGrowthDungeon(); // 씬 진입시 성장형 던전 초기화
+            decreaseMobCount.AddListener(DecreaseMobCount); // 몹 감소 이벤트 등록
         }
         else
         {
@@ -44,7 +44,7 @@ public class GrowthLevelManager : MonoBehaviour
         }
     }
 
-    private void DecreaseMobCount()
+    private void DecreaseMobCount() // 몬스터 감소 이벤트, 일정 횟수 이상 몹을 해치우면 보스 생성 등 로직 추가 가능
     {
         if (curLevelMobCount > 0)
         {
@@ -57,11 +57,11 @@ public class GrowthLevelManager : MonoBehaviour
         }
     }
 
-    private void InitGrowthDungeon()
+    private void InitGrowthDungeon() // 씬 진입 시 성장형 던전 초기화용
     {
-        maze_spawnPoint.transform.position += new Vector3(mazeIndent >> 1, 0, mazeIndent >> 1);
+        // maze_spawnPoint.transform.position += new Vector3(mazeIndent >> 1, 0, mazeIndent >> 1);
         // level 0
-        worldLevel = -1;
+        worldLevel = 0;
 
         if (Player.Instance == null)
             Instantiate(playerPrefab);
@@ -73,31 +73,40 @@ public class GrowthLevelManager : MonoBehaviour
         TeleportPlayer(playerSpawnPoint);
     }
 
-    private void UpdateNavMesh()
+    private void UpdateNavMesh() // 네브메시 업데이트
     {
         maze_parent_nav.UpdateNavMesh(maze_parent_nav.navMeshData);
     }
 
-    public void UpdateNavMeshDelay(float sec)
+    public void UpdateNavMeshDelay(float sec) // 네브메시 업데이트를 오브젝트들의 생성/삭제가 끝난 이후에 해야하므로 지연실행 시킴
     {
         Invoke("UpdateNavMesh", sec);
     }
 
     [Button]
-    private void MakeRandomMap()
+    public void NextLevel() // 해당 레벨 클리어 후 다음 레벨 진입
     {
         worldLevel++;
         curLevelMobCount = 0;
 
-        curWorldMapType = UnityEngine.Random.Range(0, 2);
-        curWorldMapType = 1;
-        if (curWorldMapType == 0) // 던전1
+        MakeRandomMap();
+        
+        TeleportPlayer(playerSpawnPoint);
+    }
+
+    [Button]
+    private void MakeRandomMap() // 랜덤으로 던전1 및 미로던전으로 진입
+    {
+        
+        curWorldMapType = UnityEngine.Random.Range(0, 10);
+        // curWorldMapType = 1;
+        if (curWorldMapType < 4) // 던전1
         {
             playerSpawnPoint = dungeon1_spawnPoint.position;
         }
         else // 미로 랜덤 생성
         {
-            playerSpawnPoint = maze_spawnPoint.position;
+            // playerSpawnPoint = maze_spawnPoint.position;
             if (!ReferenceEquals(randomMazeGenerator, null)) // 기존 미로 삭제
             {
                 randomMazeGenerator.Terminate();
@@ -107,6 +116,7 @@ public class GrowthLevelManager : MonoBehaviour
 
             randomMazeGenerator = new RandomMazeGenerator(maze_parent.transform, maze_parent.transform.position, mazeIndent, maxMazeBlockCount);
             randomMazeGenerator.RandomGenerate();
+            playerSpawnPoint = randomMazeGenerator.PlayerSpawnPoint().position;
             
             if (maze_parent_nav.navMeshData == null)
             {
@@ -123,7 +133,7 @@ public class GrowthLevelManager : MonoBehaviour
         // playerSpawnPoint = dungeon1_spawnPoint.position;
     }
 
-    private void TeleportPlayer(Vector3 pos)
+    private void TeleportPlayer(Vector3 pos) // 플레이어 위치 이동 (에이전트 on/off)
     {
         if (Player.Instance == null)
         {
--- Assets/Scripts/Managers/Dungeon/MazeComponent.cs
+++ Assets/Scripts/Managers/Dungeon/MazeComponent.cs
@@ -7,32 +7,29 @@ using UnityEngine;
 
 public class MazeComponent : MonoBehaviour
 {
-    public List<Transform> monsterSpawnPoints = new List<Transform>();
-    [SerializeField][Required] private Transform mid_point;
+    [SerializeField][Required] private Transform playerSpawnPoint; // 이 미로 블럭 내에서 플레이어가 스폰될 수 있는 위치
+    public List<Transform> monsterSpawnPoints = new List<Transform>(); // 몬스터를 스폰할 위치 좌표들
+    [SerializeField][Required] private Transform mid_point; // 이 미로 블럭의 정중앙 위치값, 미로 블럭 클리어 시 4방향의 벽을 부수기 위함
 
     private int[] dy = new int[4];
     private int[] dx = new int[4];
     
-    public int SpawnMonsters()
+    public int SpawnMonsters() // 몬스터들을 생성하고 마릿수를 반환 (일정 %의 몹 제거시 이벤트 활용을 위함)
     {
         
         return 0;
     }
 
-    private RandomMazeGenerator generator;
-    public void BuildWalls(RandomMazeGenerator generator)
+    // private RandomMazeGenerator generator; // dy, dx를 가져오기 위한 변수
+    public void BuildWalls(RandomMazeGenerator generator) // 미로 블럭 기준으로 처음에는 4방향 진로를 모두 막아둠
     {
-        this.generator = generator;
+        // this.generator = generator;
         Array.Copy(generator.dy, dy, 4);
         Array.Copy(generator.dx, dx, 4);
-        // this.dy = generator.dy;
-        // this.dx = generator.dx;
         
         GameObject wallgo = Resources.Load("MazeComponents/Wall/Wall").GameObject();
         for (int i = 0; i < 4; i++)
         {
-            // dy[i] = dy[i] >> 1;
-            // dx[i] = dx[i] >> 1;
             Vector3 wallPos = mid_point.position + new Vector3(generator.dx[i] >> 1, 0, generator.dy[i] >> 1);
             GameObject newWall = Instantiate(wallgo, wallPos, Quaternion.LookRotation(wallPos - mid_point.position));
             newWall.name = "wall " + this.gameObject.name + i.ToString();
@@ -42,7 +39,7 @@ public class MazeComponent : MonoBehaviour
     }
 
     [Button]
-    public void CollapseWalls()
+    public void CollapseWalls() // 미로 블럭 내 모든 몬스터 제거시 방 오픈?
     {
         RaycastHit[] hits;
         for (int i = 0; i < 4; i++)
@@ -58,7 +55,12 @@ public class MazeComponent : MonoBehaviour
         GrowthLevelManager.Instance.UpdateNavMeshDelay(Time.deltaTime * 1.5f);
     }
 
-    public void Terminate()
+    public Transform ReturnPlayerSpawnPoint() // 현재 블럭에서 플레이어가 스폰될 수 있는 위치를 반환
+    {
+        return playerSpawnPoint;
+    }
+
+    public void Terminate() // 미로 블럭 셧다운
     {
         Destroy(this.gameObject);
     }
